58e72c1f1cff21167ae5ac6f31138e0e286f6e17
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2026-04-18 13:06:38 +0100

    minor removal

--- config/settings.py
+++ config/settings.py
@@ -109,14 +109,6 @@ if os.environ.get('DATABASE_URL'):
         )
     }
     DATABASES['default']['ATOMIC_REQUESTS'] = True
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
